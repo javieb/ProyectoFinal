8228acdf3bc6e5bd8b2cb5b4879cfb681d2b4a59
Javi <jesmorisb22@fpcoruna@afundacion.org>
2024-05-29 01:06:44 +0200

    Endpoint get notifications #14
    Se añade el endpoint para conseguir las notificaciones enviadas y recibidas de un usuario.

--- backend/APIREST/API_Miempleado/app_miempleado/urls.py
+++ backend/APIREST/API_Miempleado/app_miempleado/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register),
     path('sign-in/', views.sign_in),
     path('sign-out/', views.sign_out),
-    path('holidays-absences/', views.holidaysNabsences)
+    path('holidays-absences/', views.holidaysNabsences),
+    path('notifications/', views.notifications)
 ]
