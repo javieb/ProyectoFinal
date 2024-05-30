6e4ebb390fec8fdf19f8b76ec1eec7d46930354c
Javi <jesmorisb22@fpcoruna@afundacion.org>
2024-05-30 18:37:40 +0200

    Endpoint last access #18
    Se añade el endpoint devolver el último registro horario de un empleado.

--- backend/APIREST/API_Miempleado/app_miempleado/urls.py
+++ backend/APIREST/API_Miempleado/app_miempleado/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('sign-in/', views.sign_in),
     path('sign-out/', views.sign_out),
     path('holidays-absences/', views.holidaysNabsences),
-    path('notifications/', views.notifications)
+    path('notifications/', views.notifications),
+    path('lastAccess/', views.lastAccess),
 ]
